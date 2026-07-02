60c6cf9e3dd89ac2e5dc746b41b7938cb5173cf4
Dominic Heutelbeck <dominic@heutelbeck.com>
2026-07-02 16:32:31 +0200

    remove deprecated API

--- blazon-spring/src/main/java/com/heutelbeck/blazon/spring/BlazonBannerEnvironmentPostProcessor.java
+++ blazon-spring/src/main/java/com/heutelbeck/blazon/spring/BlazonBannerEnvironmentPostProcessor.java
@@ -11,9 +11,9 @@ package com.heutelbeck.blazon.spring;
 import java.util.Locale;
 import java.util.Optional;
 
+import org.springframework.boot.EnvironmentPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.properties.bind.Binder;
-import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 
